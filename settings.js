cbd8ff1487004c4c9f5c2c06d27f7c42ef50de3c
EMONKUN28 <ilmansenpai@gmail.com>
2024-06-18 00:56:31 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -19,19 +19,19 @@ global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = 'ItsMeBotz' //ur bot name
+global.ownernumber = '6283165803679' //ur owner number
+global.ownername = 'IlmanRf' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
 global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "ItsMeBotz"
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "🦄드림 가이 Xeon\n\n6283165803679"
+global.creator = "6283165803679@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["6283165803679"] // Premium User
 global.hituet = 0
 
 //bot sett
